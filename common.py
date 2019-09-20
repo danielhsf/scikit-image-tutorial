5242b1a46458f2097b406ad200f5cdb8c37f710f
danielhsf <eng.danielhsfernandes@gmail.com>
2019-09-20 17:14:23 -0300

    gitando antes da formatação

--- common.py
+++ common.py
@@ -7,3 +7,12 @@ def show_image(image,Title="Image",cmap_type="gray"):
     plt.title(Title)
     plt.axis('off')
     plt.show()
+
+def plot_comparison(original,filtered,title_filtered):
+    fig, (ax1,ax2) = plt.subplots(ncols = 2,figsize = (8,6), sharex=True,sharey=True)
+    ax1.imshow(original,cmap=plt.cm.gray)
+    ax1.set_title('original')
+    ax1.axis('off')
+    ax2.imshow(filtered,cmap=plt.cm.gray)
+    ax2.set_title(title_filtered)
+    ax2.axis('off')
